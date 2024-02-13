0615d0e4730e489efeed39cc1c4ea62f48cd4c6f
najim2004 <najim10112004@gmail.com>
2024-02-13 10:47:41 +0600

    Added calculate sense

--- js/script.js
+++ js/script.js
@@ -1,15 +1,58 @@
+function getInputValue(inputFieldId){
+    const inputField=document.getElementById(inputFieldId);
+    const inputValue=parseFloat(inputField.value);
+    return inputValue;
+}
+
+function setInnerText(elementId, area){
+    const element=document.getElementById(elementId);
+    element.innerText=area;
+}
+
 function calculateTriangleArea(){
-    const triangleBaseInput=document.getElementById('tb');
-    const triangleHeightInput=document.getElementById('th');
-    const b=parseFloat(triangleBaseInput.value);
-    const h=parseFloat(triangleHeightInput.value);
+    const b=getInputValue('tb');
+    const h=getInputValue('th');
     const area=0.5*b*h;
+    setInnerText('tarea', area);
 }
 
 function calculateRectangleArea(){
-    const rectangleWidthInput=document.getElementById('rw');
-    const rectangleLengthInput=document.getElementById('rl');
-    const w=parseFloat(rectangleWidthInput.value);
-    const l=parseFloat(rectangleLengthInput.value);
+    const w=getInputValue('rw');
+    const l=getInputValue('rl');
     const area=w*l;
+    setInnerText('rarea', area);
+}
+
+// function calculateTriangleArea(){
+//     const ParallelogramBaseInput=document.getElementById('pb');
+//     const ParallelogramHeightInput=document.getElementById('ph');
+//     const b=parseFloat(ParallelogramBaseInput.value);
+//     const h=parseFloat(ParallelogramHeightInput.value);
+//     const area=0.5*b*h;
+// }
+
+function calculateParallelogramArea(){
+    const b=getInputValue('pb');
+    const h=getInputValue('ph');
+    const area=b*h;
+    setInnerText('parea', area);
+}
+
+function calculateRhombusArea(){
+    const d1=getInputValue('rd1');
+    const d2=getInputValue('rd2');
+    const area=0.5*d1*d2;
+    setInnerText('rharea', area);
+}
+function calculatePentagonArea(){
+    const p=getInputValue('pep');
+    const b=getInputValue('peb');
+    const area=0.5*p*b;
+    setInnerText('pearea', area);
+}
+function calculateEllipseArea(){
+    const a=getInputValue('ea');
+    const b=getInputValue('eb');
+    const area=Math.PI*a*b;
+    setInnerText('earea', area);
 }
